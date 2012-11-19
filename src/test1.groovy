f05b2563987fdb8430c2a4f0b3c45d6125257f9b
Leonardo Zacharewicz <leonardo.zacharewicz@mercadolibre.com>
2012-11-19 18:19:28 -0300

    Testing repo configuration

---
+++ src/test1.groovy
@@ -0,0 +1,10 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lzacharewicz
+ * Date: 19/11/12
+ * Time: 6:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class test1 {
+}
