18b02ac8cab435e6a76992b907dff685abd9b322
NeuronikaSoft <neuronikasoft@neuronikasoft.com>
2012-11-19 18:23:58 -0300

    Testing repo config, second commit

--- src/test1.groovy
+++ src/test1.groovy
@@ -1,10 +1,11 @@
 
 /**
  * Created with IntelliJ IDEA.
- * User: lzacharewicz
+ * User: NeuronikaSoft
  * Date: 19/11/12
  * Time: 6:18 PM
  * To change this template use File | Settings | File Templates.
  */
 class test1 {
+
 }
